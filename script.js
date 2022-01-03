f63554a39e6f4e8cdb8bbccb47fd685796489243
Parker Hagen <95255801+parkerhagen@users.noreply.github.com>
2022-01-03 23:10:38 +0000

    Added Global Variables to JavaScript.

---
+++ script.js
@@ -0,0 +1,10 @@
+//Global variables
+var canvas = document.getElementById("mrCanvas");
+var context = cnavas.getContext("2d");
+
+var scoreText = document.getElementById("score");
+var score = 0;
+
+var enemySubs = [];
+var things = [];
+var waterLevel = 50;
